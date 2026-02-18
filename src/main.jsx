f3aeec6c13eff0d702b77187dbf14084c666d1bb
velmuruganff2006 <velmuruganff2006@gmail.com>
2026-02-18 17:41:04 +0530

    perumal login page remove

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import Login from './components/Login.jsx'
-import Admin from './components/Admin.jsx'
-import UploadPhoto from './pages/Experience.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/admin" element={<Admin />} />
-      <Route path="/upload-photo" element={<UploadPhoto />} />
     </Routes>
   </BrowserRouter>
 )
